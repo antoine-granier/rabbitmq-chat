2035a38bd4d07e1de4655607b58b27796a8d38e4
Your NameDany <you@example.comdanydc1@hotmail.fr>
2024-04-03 15:06:29 +0200

    add modal to auth user

--- backend/server.js
+++ backend/server.js
@@ -1,93 +1,103 @@
-const amqp = require('amqplib/callback_api')
+const amqp = require("amqplib/callback_api");
 
-let userList = []
-let messagesList = []
+let userList = [];
+let messagesList = [];
 
 const randomId = () => {
-    return Math.floor(Math.random() * 100) * Date.now()
-}
+  return Math.floor(Math.random() * 100) * Date.now();
+};
 
 const io = require("socket.io")(4000, {
-    cors: {
-        origin: ['http://localhost:8080', 'http://192.168.0.22:8080']
-    }
-})
-
-io.use((socket, next) => {
-    const sessionID = socket.handshake.auth.sessionID;
-    if (sessionID) {
-        const session = userList.find(user => user.sessionID === sessionID);
-        if (session) {
-            userList = userList.map(user => {
-                if(user.sessionID === sessionID) {
-                    return {
-                        ...user,
-                        userID: socket.id
-                    }
-                }
-                return user
-            })
-            socket.sessionID = sessionID;
-            socket.userID = socket.id;
-            socket.username = session.username;
-            return next();
-        }
-    }
-    const username = socket.handshake.auth.username;
-    if (!username) {
-        return next(new Error("invalid username"));
-    }
-    const session = {
-        sessionID: randomId(),
-        userID: socket.id,
-        username: username
-    }
-    socket.sessionID = session.sessionID;
-    socket.userID = session.userID;
-    socket.username = session.username;
-    userList.push(session)
-    next();
+  cors: {
+    origin: ["http://localhost:8080", "http://192.168.56.1:8080"],
+  },
 });
 
-io.on('connection', socket => {
-    io.to(socket.id).emit('message-history', JSON.stringify(messagesList))
-    io.to(socket.id).emit("session", {
-        sessionID: socket.sessionID,
-        userID: socket.userID,
-    });
-    amqp.connect('amqp://localhost', (err0, connection) => {
-        if (err0) {
-            console.log(err0);
-            throw err0
+io.use((socket, next) => {
+  const sessionID = socket.handshake.auth.sessionID;
+  if (sessionID) {
+    const session = userList.find(
+      (user) => user.sessionID === sessionID && user.username == socket.username
+    );
+    if (session) {
+      userList = userList.map((user) => {
+        if (user.sessionID === sessionID && user.username == socket.username) {
+          return {
+            ...user,
+            userID: socket.id,
+          };
         }
-        connection.createChannel((err1, channel) => {
-            if (err1) {
-                throw err1
-            }
-            const QUEUE_1 = 'PUSH'
-            const QUEUE_2 = 'PULL'
-            channel.assertQueue(QUEUE_1)
-            channel.assertQueue(QUEUE_2)
-            socket.on('PUBLISH', message => {
-                const data = {
-                    message,
-                    user: userList.find(user => user.sessionID === socket.sessionID)
-                }
-                messagesList.push(data)
-                channel.sendToQueue(QUEUE_1, Buffer.from(JSON.stringify(data)))
-                io.to(data.user.userID).emit('SUBSCRIBE', `Me: ${data.message}`)
+        return user;
+      });
+      socket.sessionID = sessionID;
+      socket.userID = socket.id;
+      socket.username = session.username;
+      return next();
+    }
+  }
+  const username = socket.handshake.auth.username;
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
+  const session = {
+    sessionID: randomId(),
+    userID: socket.id,
+    username: username,
+  };
 
-            })
-            channel.consume(QUEUE_2, (message) => {
-                const data = JSON.parse(message.content.toString())
-                userList.filter(user => user.sessionID !== data.user.sessionID).forEach(user => {
-                    io.to(user.userID).emit('SUBSCRIBE', `${data.user.username}: ${data.message}`)
-                })
-            }, { noAck: true })
-        })
-    })
+  socket.sessionID = session.sessionID;
+  socket.userID = session.userID;
+  socket.username = session.username;
+  userList.push(session);
+  next();
+});
 
-})
+io.on("connection", (socket) => {
+  io.to(socket.id).emit("message-history", JSON.stringify(messagesList));
+  io.to(socket.id).emit("session", {
+    sessionID: socket.sessionID,
+    userID: socket.userID,
+  });
+  amqp.connect("amqp://localhost", (err0, connection) => {
+    if (err0) {
+      console.log(err0);
+      throw err0;
+    }
+    connection.createChannel((err1, channel) => {
+      if (err1) {
+        throw err1;
+      }
+      const QUEUE_1 = "PUSH";
+      const QUEUE_2 = "PULL";
+      channel.assertQueue(QUEUE_1);
+      channel.assertQueue(QUEUE_2);
+      socket.on("PUBLISH", (message) => {
+        const data = {
+          message,
+          user: userList.find((user) => user.sessionID === socket.sessionID),
+        };
+        messagesList.push(data);
+        channel.sendToQueue(QUEUE_1, Buffer.from(JSON.stringify(data)));
+        io.to(data.user.userID).emit("SUBSCRIBE", `Me: ${data.message}`);
+      });
+      channel.consume(
+        QUEUE_2,
+        (message) => {
+          const data = JSON.parse(message.content.toString());
+          userList
+            .filter((user) => user.sessionID !== data.user.sessionID)
+            .forEach((user) => {
+              io.to(user.userID).emit(
+                "SUBSCRIBE",
+                `${data.user.username}: ${data.message}`
+              );
+            });
+        },
+        { noAck: true }
+      );
+    });
+  });
+});
 
 // io.on("connect", (socket) => {
 //     if(!userList.find(user => user == socket.id)) {
--- frontend/socket.js
+++ frontend/socket.js
@@ -1,56 +1,66 @@
-import { io } from 'socket.io-client'
+import { io } from "socket.io-client";
 
-const socket = io('http://localhost:4000', {autoConnect: false})
+const socket = io("http://localhost:4000", { autoConnect: false });
 
-const chat = document.querySelector('.container')
-const input = document.querySelector('#message')
-const form = document.querySelector('.controller')
-const body = document.getElementsByTagName("body")[0]
+const chat = document.querySelector(".container");
+const input = document.querySelector("#message");
+const form = document.querySelector(".controller");
+const body = document.getElementsByTagName("body")[0];
 
 body.onload = () => {
-    const sessionID = localStorage.getItem("sessionID");
+  const sessionID = localStorage.getItem("sessionID");
+  const form = document.getElementById("username-form");
+  const input = document.getElementById("username-input");
 
-    // if (sessionID) {
-        socket.auth = { sessionID: Number(sessionID), username: "totot" };
-        socket.connect();
-    // }
-}
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (input.value) {
+      localStorage.setItem("username", input.value);
+      form.style.display = "none";
+      socket.auth = { sessionID: Number(sessionID), username: input.value };
+      socket.connect();
+    }
+  });
+};
 
 const sendMessage = (message) => {
-    const text = document.createElement('p')
-    text.innerText = message
-    chat.appendChild(text)
-}
+  const text = document.createElement("p");
+  text.innerText = message;
+  chat.appendChild(text);
+};
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    if (input.value) {
-        socket.emit("PUBLISH", input.value)
-    }
-    input.value = ''
-})
-
+  if (input.value) {
+    socket.emit("PUBLISH", input.value);
+  }
+  input.value = "";
+});
 
 // socket.on("connect", () => {
 //     sendMessage(`Connected with ID: ${socket.id}`)
 // })
 
 socket.on("SUBSCRIBE", (message) => {
-    sendMessage(message)
-})
+  sendMessage(message);
+});
 
 socket.on("message-history", (messageHistory) => {
-    const messages = JSON.parse(messageHistory)
-    const sessionID = Number(localStorage.getItem("sessionID"))
-    chat.replaceChildren()
-    messages.forEach(message => {
-        sendMessage(`${message.user.sessionID === sessionID ? "Me" : message.user.username}: ${message.message}`)
-    })
-})
+  const messages = JSON.parse(messageHistory);
+  const sessionID = Number(localStorage.getItem("sessionID"));
+  chat.replaceChildren();
+  messages.forEach((message) => {
+    sendMessage(
+      `${
+        message.user.sessionID === sessionID ? "Me" : message.user.username
+      }: ${message.message}`
+    );
+  });
+});
 
-socket.on("session", ({sessionID, userID}) => {
-    socket.auth = { sessionID };
-    localStorage.setItem("sessionID", sessionID);
-    socket.userID = userID;
+socket.on("session", ({ sessionID, userID }) => {
+  socket.auth = { sessionID };
+  localStorage.setItem("sessionID", sessionID);
+  socket.userID = userID;
 });
